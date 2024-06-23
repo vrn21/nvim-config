0068c303db3cf714b34e8c6deada7a0adc23bc33
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-06-23 20:44:55 +0530

    updated neovide font size

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMonoNL Nerd Font:h15" -- text below applies for VimScript
+end
